a093b9b331222360997b5ec7d0ed45afa3307d94
onofree28 <onofre_luiz@hotmail.com>
2022-10-19 08:08:01 -0300

    Em desenvolvimento

--- src/views/Animes/DemonSlayer.vue
+++ src/views/Animes/DemonSlayer.vue
@@ -1,6 +1,12 @@
 <template>
-    <div class="banner">
-        <img src="/banner-pagina/demon-slayer-banner-pagina.jpeg" alt="demon-slayer-banner">
+    <div>
+        <div class="banner">
+            <img src="/banner-pagina/demon-slayer-banner-pagina.jpg" alt="demon-slayer-banner">
+        </div>
+        <div class="info">
+            <h1>Demon Slayer: Kimetsu no Yaiba</h1>
+        </div>
+
     </div>
 </template>
 
@@ -19,10 +25,29 @@
 
 <style scoped>
     .banner {
-        background: cover;
         display: flex;
         justify-content: center;
         align-content: center;
         background-color: #2b2b2b;
+    }
+
+    .info {
+        display: flex;
+        justify-content: space-between;
+        color: aliceblue;
+    }
+
+    @media (min-width: 300px) and (max-width: 500px) {
+        .banner img {
+            width: 300px;
+            height: 169px;
+        }
+    }
+
+    @media (min-width: 501px) and (max-width: 1000px) {
+        .banner img {
+            width: 500px;
+            height: 282px;
+        }
     }
 </style>
