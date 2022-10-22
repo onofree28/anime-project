368578ce4f3e81f5cdca402b706874aef9a464f5
onofree28 <onofre_luiz@hotmail.com>
2022-10-22 13:54:36 -0300

    atualizacao noticias

--- src/components/Noticias.vue
+++ src/components/Noticias.vue
@@ -5,14 +5,50 @@
         </div>
         <div class="barra"></div>
         <div class="container-noticias">
-
+            <div class="container-destaque">
+                <p>Noticias em destaque</p>
+                <div class="destaque" v-for="destaque in destaques" :key="destaque.id">
+                    <img :src="destaque.img" :alt="destaque.alt">
+                    <p>{{ destaque.titulo }}</p>
+                </div>
+            </div>
+            <div class="container-recentes">
+                <p>Mais recentes</p>
+                <div class="noticias">
+                    
+                </div>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
     export default {
-        name: 'Noticias'
+        name: 'Noticias',
+        data() {
+            return {
+                destaques: [
+                    {
+                        id:1,
+                        titulo: 'Calendário de Lançamentos',
+                        img: '/img-noticias/destaque1-noticia.png',
+                        alt: 'destaque-1-imagem'
+                    },
+                    {
+                        id:2,
+                        titulo: 'Conheça o elenco brasileiro e a data de estreia da dublagem de Chainsaw Man',
+                        img: '/img-noticias/destaque2-noticia.jpg',
+                        alt: 'destaque-2-imagem'
+                    }
+                ],
+
+                noticias: [
+                    {
+
+                    }
+                ]
+            }
+        }
     }
 </script>
 
@@ -45,10 +81,30 @@
     }
 
     .container-noticias {
+        display: flex;
+        justify-content: space-evenly;
+        align-items: center;
         width: 1100px;
-        height: 200px;
+        height: auto;
         padding: 20px 10px 20px 10px;
         background-color: #2b2b2b;
+    }
+
+    .container-destaque {
+        width: 300px;
+        padding: 10px 20px;
+        color: aliceblue;
+        font-size: 16px;
+    }
+
+    .destaque {
+        padding-top: 10px;
+        width: 250px;
+    }
+    .destaque img {
+        width: 250px;
+        height: 140px;
+        object-fit: cover;
     }
 
     @media (min-width: 200px) and (max-width: 760px) {
